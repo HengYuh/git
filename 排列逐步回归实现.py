a9e3cf7d2e52c0da86a490df88af19416ae3aa52
HengYu <141622455+HengYuh@users.noreply.github.com>
2023-09-27 21:48:49 +0800

    Update and rename 方法.py to 排列逐步回归实现.py

--- 排列逐步回归实现.py
+++ 排列逐步回归实现.py
@@ -1,3 +1,12 @@
+#基本思路：
+#  1.有4个被替换词，那么组合后就有A44,即4*3*2*1=24种组合，将这24种组合都进行一遍优化。
+# 2. 例如这4个被替换词为A,B,C,D,那么我们可以先假如得到一个有A1,B1,C1,D1构成的对抗性样本，我们让B1,C1,D1不变，
+# 在保持对抗性的前提下，不断替换A1，直到找到A2,使得A2,B1,C1,D1构成的样本相似度在A的所有替换词中最大，
+# 然后我们固定A2，C1,D1，重复操作得到B2,然后固定A2,B2,D2,得到C2，然后是D2,我们得到一组A2,B2,C2,D2组成的对抗性样本
+# 3. 更换顺序，例如B1,A1,C1,D1,按照这个顺序得到B2,A2,C2,D2。。。。。直到遍历24种情况
+# 4.现在我们得到24种ABCD的组合，我们比较它们构成的对抗性样本与原样本的相似度，选择相似度最高的那一组
+#如果你想知道更具体的过程，可以查阅我写代码过程中与gpt4的对话-https://chat.openai.com/share/6a000f3b-4c0f-4d33-920a-e54fde825703
+
 
     # 输入: 
     #word to replace-(a,b,c,d)
@@ -48,3 +57,5 @@ def optimized_adversarial_combinations(text_ls, replace_list, best_attack, best_
             global_best_attack = attack_temp[:]
 
     return ' '.join(global_best_attack), global_best_sim
+
+
